fd87bc89d423a60400788811cfd622bdde20d854
Drishti <yadav2006drishti@gmail.com>
2026-04-07 09:10:11 +0530

    uc5 added

--- App/src/TrainManagementApp.java
+++ App/src/TrainManagementApp.java
@@ -110,3 +110,24 @@ class UseCase4TrainConsistMgmt {
         System.out.println("\nUC4 operations completed successfully...");
     }
 }
+class UseCase5TrainConsistMgmt {
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println(" UC5 - Preserve Insertion Order of Bogies ");
+        System.out.println("========================================\n");
+
+        Set<String> formation = new LinkedHashSet<>();
+
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        formation.add("Sleeper");
+
+        System.out.println("Final Train Formation:");
+        System.out.println(formation);
+
+        System.out.println("\nUC5 operations completed successfully...");
+    }
+}
