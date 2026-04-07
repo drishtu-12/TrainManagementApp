bd438d37705499fbf61892a9549177db04d9828f
Drishti <yadav2006drishti@gmail.com>
2026-04-07 08:51:04 +0530

    uc2 added

--- App/src/TrainManagementApp.java
+++ App/src/TrainManagementApp.java
@@ -17,3 +17,37 @@ public class TrainManagementApp {
         }
     }
 }
+import java.util.*;
+
+public class TrainConsistApp {
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println("UC2 - Add Passenger Bogies to Train");
+        System.out.println("========================================\n");
+
+        List<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("After Adding Bogies:");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("\nAfter Removing 'AC Chair':");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        boolean exists = passengerBogies.contains("Sleeper");
+
+        System.out.println("\nChecking if 'Sleeper' exists:");
+        System.out.println("Contains Sleeper? : " + exists);
+
+        System.out.println("\nFinal Train Passenger Consist:");
+        System.out.println(passengerBogies);
+
+        System.out.println("\nUC2 operations completed successfully...");
+    }
+}
