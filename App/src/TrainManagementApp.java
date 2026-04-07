7219310b74a38a64fa7312a2f29601053427cfbf
Drishti <yadav2006drishti@gmail.com>
2026-04-07 08:54:33 +0530

    uc3 added

--- App/src/TrainManagementApp.java
+++ App/src/TrainManagementApp.java
@@ -51,3 +51,27 @@ public class TrainConsistApp {
         System.out.println("\nUC2 operations completed successfully...");
     }
 }
+import java.util.*;
+
+public class TrainConsistApp {
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println(" UC3 - Track Unique Bogie IDs ");
+        System.out.println("========================================\n");
+
+        Set<String> bogies = new HashSet<>();
+
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        bogies.add("BG101");
+        bogies.add("BG102");
+
+        System.out.println("Unique Bogie IDs:");
+        System.out.println(bogies);
+
+        System.out.println("\nUC3 operations completed successfully...");
+    }
+}
