e963e7cc689e3ce468b2f882d42fc38f3b6dffef
Drishti <yadav2006drishti@gmail.com>
2026-04-07 09:29:02 +0530

    uc6 added

--- App/src/TrainManagementApp.java
+++ App/src/TrainManagementApp.java
@@ -131,3 +131,25 @@ class UseCase5TrainConsistMgmt {
         System.out.println("\nUC5 operations completed successfully...");
     }
 }
+class UseCase6TrainConsistMgmt {
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println(" UC6 - Map Bogie to Capacity ");
+        System.out.println("========================================\n");
+
+        Map<String, Integer> bogieCapacity = new HashMap<>();
+
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 60);
+        bogieCapacity.put("First Class", 24);
+
+        System.out.println("Bogie Capacity Details:");
+
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        System.out.println("\nUC6 operations completed successfully...");
+    }
+}
